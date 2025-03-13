3879b9214d9366685d1736e302f4eb01680ee5a0
_Dedushka_ <zomb689@gmail.com>
2025-03-13 22:32:03 +0400

    Add files via upload

--- Metro.cpp
+++ Metro.cpp
@@ -4,70 +4,132 @@
 #include <mutex>
 #include <chrono>
 #include <thread>
+#include <vector>
+#include <map>
+
+// ну тут короче цвета
+enum class MetroLine {
+    RED,
+    GREEN,
+    PURPLE,
+    YELLOW
+};
 
 class Station {
 private:
     int id;          // ID станции
-    std::string name; // Название станции
-    std::shared_ptr<Station> next;        // ID следующей станции
-    std::shared_ptr<Station> prev;        // ID предыдущей станции
-    std::mutex mtx; //мютекс для блокировки
-    int wait_seconds;//Время ожидания на станции
-    bool depo;
+    std::string name; // Имя станции
+    std::vector<std::pair<std::shared_ptr<Station>, MetroLine>> next; // след станция и ее цвет
+    std::shared_ptr<Station> prev;        // предыдущая станция
+    std::mutex mtx; // просто мюьтекс
+    int wait_seconds; // сколько стоит на станции
+    bool depo; // это депо?
+
 public:
-    Station(int i, std::string n,int ws,bool d=false):id(i),name(n),wait_seconds(ws), next(nullptr),prev(nullptr),depo(d){};
-    //~Station();
-    bool TryArriveTrain(int train_id,bool& s_t) {
+    Station(int i, std::string n, int ws, bool d = false) 
+        : id(i), name(n), wait_seconds(ws), depo(d), prev(nullptr) {}
+
+    bool TryArriveTrain(int train_id, bool& s_t) {
         if (mtx.try_lock()) {
-            std::cout<<"Поезд с ид:"<<train_id<<" прибыл на станция  "<<name<<"("<<id<<")\n";
+            std::cout << "Train with id:" << train_id << " arrived at the station " << name << "(" << id << ")\n";
             std::this_thread::sleep_for(std::chrono::seconds(wait_seconds));
-            std::cout<<"Поезд с ид:"<<train_id<<" покинул станция "<<name<<"("<<id<<")\n";
-            if (depo==true) {
-                std::cout<<"Поезд с ид:"<<train_id<<" в депо и разворачивается.\n";
+            std::cout << "Train with id:" << train_id << " дeft the station " << name << "(" << id << ")\n";
+            if (depo) {
+                std::cout << "Train with id::" << train_id << " in the depot and turns around. \n";
                 std::this_thread::sleep_for(std::chrono::seconds(wait_seconds));
-                s_t=true;
+                s_t = true;
             }
             mtx.unlock();
             return true;
-        }else {
-            std::cout<<"Поезд с ид "<<train_id<<" ждет пока станция  "<<name<<"("<<id<<") освободится.\n";
+        } else {
+            std::cout << "Train with id: " << train_id << " waiting for the station " << name << "(" << id << ") to be released.\n";
             return false;
         }
     }
-    std::shared_ptr<Station> getNext(){ return next;}
-    std::shared_ptr<Station> getPrev(){ return prev;}
-    void setNext(std::shared_ptr<Station> n){next=n;};
-    void setPrev(std::shared_ptr<Station> p){prev=p;};
+    std::string getName(){
+        return name;
+    }
+    std::shared_ptr<Station> getPrev() { 
+        return prev; 
+    }
+
+    void setPrev(std::shared_ptr<Station> p) { 
+        prev = p; 
+    }
+
+    void addNext(std::shared_ptr<Station> n, MetroLine line) { 
+        next.push_back({n, line}); 
+    }
+
+    std::shared_ptr<Station> getNextForLine(MetroLine line) {
+        for (const auto& [station, station_line] : next) {
+            if (station_line == line) {
+                return station;
+            }
+        }
+        return nullptr; 
+    }
+    const std::vector<std::pair<std::shared_ptr<Station>, MetroLine>>& getNextStations() const {
+        return next;
+    }
 };
-class Train{
+
+class Train {
 private:
     int train_id;
     std::shared_ptr<Station> station_start;
-    bool forward;//true если вперед поедет(например выехал с депо),false если обратно(возвращается в депо в которое выехал)
-public:
-    Train(int id,std::shared_ptr<Station> s_s,bool f=true):train_id(id),station_start(s_s),forward(f){}
-    //~Train();
-    void runTrain() {
-        std::shared_ptr<Station> current_station=station_start;
-        bool should_turn=false;
-        while (current_station) {
+    bool forward; // куда движется
+    MetroLine line; // к какой ветке пренадлежит
 
-            while (!current_station->TryArriveTrain(train_id,should_turn)) {
+public:
+    Train(int id, std::shared_ptr<Station> s_s, MetroLine l, bool f = true) 
+        : train_id(id), station_start(s_s), forward(f), line(l) {}
+
+    void runTrain() {
+        std::shared_ptr<Station> current_station = station_start;
+        bool should_turn = false;
+        
+        while (current_station) {
+            while (!current_station->TryArriveTrain(train_id, should_turn)) {
                 std::this_thread::sleep_for(std::chrono::seconds(1));
             }
-            if (should_turn == true) {
+            
+            if (should_turn) {
                 forward = !forward;
-               // std::cout << "Направление поменялось на " << forward << "\n";
                 should_turn = false;
             }
 
             if (forward) {
-                current_station = current_station->getNext();
+                if (current_station->getName() == "28 Мая") {
+                    if (line == MetroLine::RED || line == MetroLine::GREEN) {
+                        current_station = current_station->getNextForLine(line);
+                    } else if (line == MetroLine::YELLOW) {
+                        current_station = current_station->getNextForLine(MetroLine::YELLOW);
+                    }
+                } else if (current_station->getName() == "Нариман Нариманов") {
+                    if (line == MetroLine::RED || line == MetroLine::GREEN) {
+                        if (train_id % 2 == 0) {
+                            for (const auto& next_station : current_station->getNextStations()) {
+                                if (next_station.first->getName() == "Кёроглу") {
+                                    current_station = next_station.first;
+                                    break;
+                                }
+                            }
+                        } else {
+                            for (const auto& next_station : current_station->getNextStations()) {
+                                if (next_station.first->getName() == "Бакмил") {
+                                    current_station = next_station.first;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                } else {
+                    current_station = current_station->getNextForLine(line);
+                }
             } else {
                 current_station = current_station->getPrev();
             }
-
         }
     }
-
 };
--- main.cpp
+++ main.cpp
@@ -1,36 +1,124 @@
 #include <iostream>
 #include "Metro.cpp"
+#include <windows.h>
+
+#ifdef _WIN32
+#include <windows.h> // Для Windows: настройка консоли
+#endif
 
 void RunMetro(Train& train) {
     train.runTrain();
 }
 
 int main() {
-    auto s1=std::make_shared<Station> (0,"Ичяри шахер",5,true);
-    auto s2=std::make_shared<Station> (1,"Сахиль",1,false);
-    auto s3=std::make_shared<Station> (2,"28 Мая",3,false);
-    auto s4=std::make_shared<Station> (3,"Гянджлик",1,false);
-    auto s5=std::make_shared<Station> (4,"Нариман Нариманов",1,false);
-    auto s6=std::make_shared<Station> (5,"Бакмиль",5,true);
-    s1->setNext(s2);
-    s1->setPrev(s2);
-    s2->setPrev(s1);
-    s2->setNext(s3);
-    s3->setPrev(s2);
-    s3->setNext(s4);
-    s4->setPrev(s3);
-    s4->setNext(s5);
-    s5->setPrev(s4);
-    s5->setNext(s6);
-    s6->setPrev(s5);
-    s6->setNext(s5);
-    Train train1(1,s1,false);
-    Train train2(2,s6,true);
-    Train train3(3,s4,false);
-    Train train4(4,s2,true);
-    std::thread t1(RunMetro,std::ref(train1));
-    std::thread t2(RunMetro,std::ref(train2));
-    t1.join();
-    t2.join();
+    auto hodjasan = std::make_shared<Station>(1, "Ходжасан", 2);
+    auto avtovokzal = std::make_shared<Station>(2, "Автовокзал", 2);
+    auto memar_adjemi = std::make_shared<Station>(3, "Мемар Аджеми", 2);
+    auto yanvarya = std::make_shared<Station>(4, "20 Ноября", 2);
+    auto noyabra = std::make_shared<Station>(5, "8 Ноября", 2);
+    auto inshaatchylar = std::make_shared<Station>(6, "Иншаатчылар", 2);
+    auto elmlar_akademiyasy = std::make_shared<Station>(7, "Элмлар Академиясы", 2);
+    auto niizami = std::make_shared<Station>(8, "Низами", 2);
+    auto sahil = std::make_shared<Station>(9, "Сахил", 2);
+    auto icheri_sheher = std::make_shared<Station>(10, "Ичери Шехер", 2);
+    auto _may = std::make_shared<Station>(11, "28 Мая", 2);
+    auto gandzhlik = std::make_shared<Station>(12, "Гянджлик", 2);
+    auto nariman_narimanov = std::make_shared<Station>(13, "Нариман Нариманов", 2);
+    auto ulduz = std::make_shared<Station>(14, "Улдуз", 2);
+    auto bakmil = std::make_shared<Station>(15, "Бакмил", 2, true); // Depot
+    auto koroğlu = std::make_shared<Station>(16, "Кёроглу", 2);
+    auto qara_qarayev = std::make_shared<Station>(17, "Кара Караев", 2);
+    auto neftchilar = std::make_shared<Station>(18, "Нефтчиляр", 2);
+    auto xalqlar_dostluqu = std::make_shared<Station>(19, "Халглар Достлугу", 2);
+    auto ahmedly = std::make_shared<Station>(20, "Ахмедлы", 2);
+    auto azi_aslanov = std::make_shared<Station>(21, "Ази Асланов", 2, true); // Depot
+    auto djabar_djabbarly = std::make_shared<Station>(22, "Джафар Джаббарлы", 2);
+    auto shah_islam_khatayi = std::make_shared<Station>(23, "Шах Исмаил Хатаи", 2, true); // Depot
+    auto dargahli = std::make_shared<Station>(24, "Дарнагюль", 2);
+    auto azatlyg = std::make_shared<Station>(25, "Азадлыг", 2);
+    auto nasimi = std::make_shared<Station>(26, "Насими", 2);
+    // Фиолетовая линия: Ходжасан → Автовокзал → Мемар Аджеми → 8 Ноября
+    hodjasan->addNext(avtovokzal, MetroLine::PURPLE);
+    avtovokzal->setPrev(hodjasan);
+    avtovokzal->addNext(memar_adjemi, MetroLine::PURPLE);
+    memar_adjemi->setPrev(avtovokzal);
+    memar_adjemi->addNext(noyabra, MetroLine::PURPLE);
+    noyabra->setPrev(memar_adjemi);
+
+    //Зеленая линия: Дарнагюль → Азатлыг → Насими → Мемар Аджеми → 20 Ноября → Иншаатчылар → Элмляр Академиясы → Низами → 28 Мая → Гянджлик → Нариман Нариманов → (Бакмил or Кёроглу) → Кара Караев → Нефтчиляр → Халглар Достлугу → Ахмедлы → Ази Асланов
+    dargahli->addNext(azatlyg, MetroLine::GREEN);
+    azatlyg->setPrev(dargahli);
+    azatlyg->addNext(nasimi, MetroLine::GREEN);
+    nasimi->setPrev(azatlyg);
+    nasimi->addNext(memar_adjemi, MetroLine::GREEN);
+    memar_adjemi->addNext(noyabra, MetroLine::GREEN);
+    noyabra->setPrev(memar_adjemi);
+    noyabra->addNext(inshaatchylar, MetroLine::GREEN);
+    inshaatchylar->setPrev(noyabra);
+    inshaatchylar->addNext(elmlar_akademiyasy, MetroLine::GREEN);
+    elmlar_akademiyasy->setPrev(inshaatchylar);
+    elmlar_akademiyasy->addNext(niizami, MetroLine::GREEN);
+    niizami->setPrev(elmlar_akademiyasy);
+    niizami->addNext(_may, MetroLine::GREEN);
+    _may->setPrev(niizami);
+
+    // Крассная линия: Ичери Шехер → Сахил → 28 Мая → Гянджлик → Нариман Нариманов → (Бакмил or Кёроглу) → Кара Караев → Нефтчиляр → Халглар Достлугу → Ахмедлы → Ази Асланов
+    icheri_sheher->addNext(sahil, MetroLine::RED);
+    sahil->setPrev(icheri_sheher);
+    sahil->addNext(_may, MetroLine::RED);
+    _may->setPrev(sahil);
+
+    // Момент с нариманово: 28 Мая → Гянджлик → Нариман Нариманов → (branches to Бакмил or Кёроглу)
+    _may->addNext(gandzhlik, MetroLine::RED);
+    _may->addNext(gandzhlik, MetroLine::GREEN);
+    _may->addNext(djabar_djabbarly, MetroLine::YELLOW); // Branch to Yellow Line
+    gandzhlik->setPrev(_may);
+    gandzhlik->addNext(nariman_narimanov, MetroLine::RED);
+    gandzhlik->addNext(nariman_narimanov, MetroLine::GREEN);
+    nariman_narimanov->setPrev(gandzhlik);
+    nariman_narimanov->addNext(bakmil, MetroLine::RED); // Branch 1
+    nariman_narimanov->addNext(bakmil, MetroLine::GREEN);
+    nariman_narimanov->addNext(koroğlu, MetroLine::RED); // Branch 2
+    nariman_narimanov->addNext(koroğlu, MetroLine::GREEN);
+    bakmil->setPrev(nariman_narimanov);
+    koroğlu->setPrev(nariman_narimanov);
+
+    // После кероглу: Кёроглу → Кара Караев → Нефтчиляр → Халглар Достлугу → Ахмедлы → Ази Асланов
+    koroğlu->addNext(qara_qarayev, MetroLine::RED);
+    koroğlu->addNext(qara_qarayev, MetroLine::GREEN);
+    qara_qarayev->setPrev(koroğlu);
+    qara_qarayev->addNext(neftchilar, MetroLine::RED);
+    qara_qarayev->addNext(neftchilar, MetroLine::GREEN);
+    neftchilar->setPrev(qara_qarayev);
+    neftchilar->addNext(xalqlar_dostluqu, MetroLine::RED);
+    neftchilar->addNext(xalqlar_dostluqu, MetroLine::GREEN);
+    xalqlar_dostluqu->setPrev(neftchilar);
+    xalqlar_dostluqu->addNext(ahmedly, MetroLine::RED);
+    xalqlar_dostluqu->addNext(ahmedly, MetroLine::GREEN);
+    ahmedly->setPrev(xalqlar_dostluqu);
+    ahmedly->addNext(azi_aslanov, MetroLine::RED);
+    ahmedly->addNext(azi_aslanov, MetroLine::GREEN);
+    azi_aslanov->setPrev(ahmedly);
+
+    // Желтая линия: Джафар Джаббарлы → Шах Исмаил Хатаи
+    djabar_djabbarly->setPrev(shah_islam_khatayi);
+    djabar_djabbarly->addNext(shah_islam_khatayi, MetroLine::YELLOW);
+    shah_islam_khatayi->setPrev(djabar_djabbarly);
+    shah_islam_khatayi->addNext(djabar_djabbarly, MetroLine::YELLOW);
+
+    Train train_red(1, icheri_sheher, MetroLine::RED);
+    Train train_green(2, dargahli, MetroLine::GREEN);
+    Train train_purple(3, hodjasan, MetroLine::PURPLE);
+    Train train_yellow(4, djabar_djabbarly, MetroLine::YELLOW);
+
+    /*std::thread red_thread(&Train::runTrain, &train_red);
+    std::thread green_thread(&Train::runTrain, &train_green);*/
+    std::thread purple_thread(&Train::runTrain, &train_purple);
+    std::thread yellow_thread(&Train::runTrain, &train_yellow);
+
+   /* red_thread.join();
+    green_thread.join();*/
+    purple_thread.join();
+    yellow_thread.join();
     return 0;
 }
